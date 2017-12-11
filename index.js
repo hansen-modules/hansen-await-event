8a8e0e2ee536bb661a080cdb9f03c8318f913e22
uwx <chris331hansen@outlook.com>
2017-12-11 21:12:06 -0200

    only resolve once

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = (emitter, event) => {
   new Promise((resolve, reject) => {
-    emitter.on(event, (...args) => {
+    emitter.once(event, (...args) => {
       resolve(...args);
     });
   });
